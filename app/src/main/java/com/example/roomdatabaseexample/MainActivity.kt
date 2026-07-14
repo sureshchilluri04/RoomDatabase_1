7b8c04a1065d587b12651b4284ce0dcae78781cc
vchilluri <suresh_chilluri@intuit.com>
2026-07-13 17:48:36 -0700

    first commit

---
+++ app/src/main/java/com/example/roomdatabaseexample/MainActivity.kt
@@ -0,0 +1,113 @@
+package com.example.roomdatabaseexample
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.roomdatabaseexample.ui.theme.RoomDatabaseExampleTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+
+    var count = 1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RoomDatabaseExampleTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                    val noteDatabase = NoteDatabase.getDatabase(this@MainActivity)
+
+                    Column(modifier = Modifier.fillMaxSize().padding(innerPadding),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally) {
+
+                        Button(onClick = {
+                            // Insert a new note
+                            CoroutineScope(Dispatchers.IO).launch {
+                                val newNote = Note(title = "My Note $count", content = "This is a sample note $count")
+                                noteDatabase.noteDao().insert(newNote)
+                                count++
+                            }
+                        }) {
+                            Text("Insert")
+                        }
+
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        Button(onClick = {
+                            CoroutineScope(Dispatchers.IO).launch {
+                                val existingNote = noteDatabase.noteDao().getAllNotes()
+                                existingNote?.let {
+                                    existingNote.forEach {
+                                        note->
+                                        note.title = "Updated Note"
+                                        noteDatabase.noteDao().update(note)
+                                    }
+                                }
+                            }
+                        }) {
+                            Text("Update")
+                        }
+
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        Button(onClick = {
+                            CoroutineScope(Dispatchers.IO).launch {
+                                val existingNote = noteDatabase.noteDao().getAllNotes()
+                                existingNote?.let {
+                                    existingNote.forEach {
+                                            note->
+                                        noteDatabase.noteDao().delete(note)
+                                    }
+                                }
+                            }
+                        }) {
+                            Text("Delete")
+                        }
+
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        Button(onClick = {
+
+                            CoroutineScope(Dispatchers.IO).launch {
+                                val existingNote = noteDatabase.noteDao().getAllNotes()
+                                existingNote?.let {
+                                    existingNote.forEach {
+                                            note->
+                                        Log.i("AppLogs","${note.id} ${note.title} ${note.content}")
+                                    }
+                                }
+                            }
+
+                            Log.i("AppLogs","===========================================")
+                        }) {
+                            Text("Query")
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/roomdatabaseexample/NoteDao.kt
@@ -0,0 +1,23 @@
+package com.example.roomdatabaseexample
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface NoteDao {
+    @Insert
+    suspend fun insert(note: Note)
+
+    @Update
+    suspend fun update(note: Note)
+
+    @Delete
+    suspend fun delete(note: Note)
+
+    @Query("SELECT * FROM notes")
+    fun getAllNotes(): List<Note>
+}
